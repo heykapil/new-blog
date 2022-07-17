5270c0b7c6d5a7188485a72b663adc3193f84bb3
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-07-18 03:26:47 +0530

    fixed-build

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import Image from 'next/future/image';
 
 import ProsCard from 'components/ProsCard';
 import ConsCard from 'components/ConsCard';
+import Unsplash from 'components/metrics/Unsplash';
 import Analytics from 'components/metrics/Analytics';
+import YouTube from 'components/metrics/Youtube';
 import Step from 'components/Step';
 import ImageWithTheme from 'components/ImageWithTheme';
+
 const CustomLink = (props) => {
   const href = props.href;
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
@@ -27,7 +30,7 @@ function RoundedImage(props) {
 
 function Callout(props) {
   return (
-    <div className="flex bg-gray-200 dark:bg-gray-800 rounded-lg p-4">
+    <div className="flex bg-gray-200 dark:bg-gray-800 rounded-lg p-4 my-8">
       <div className="flex items-center w-4 mr-4">{props.emoji}</div>
       <div className="w-full callout">{props.children}</div>
     </div>
@@ -42,7 +45,9 @@ const MDXComponents = {
   Analytics,
   ConsCard,
   ProsCard,
-  Step
+  Step,
+  Unsplash,
+  YouTube
 };
 
 export default MDXComponents;
--- lib/types.ts
+++ lib/types.ts
@@ -37,6 +37,16 @@ export type TopTracks = {
   tracks: Song[];
 };
 
+ export type YouTube = {
+   subscriberCount: number;
+   viewCount: number;
+ };
+
 export type GitHub = {
   stars: number;
+};
+
+ export type Unsplash = {
+   downloads: number;
+   views: number;
 };
