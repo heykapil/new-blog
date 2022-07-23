3dd1fab17f72453e2e9186c2bfbe5acb769346a0
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2022-07-23 12:13:08 +0530

    Update Guestbook.tsx

--- components/Guestbook.tsx
+++ components/Guestbook.tsx
@@ -3,7 +3,7 @@ import { useState, useRef, Suspense } from 'react';
 import { format } from 'date-fns';
 import { signIn, useSession } from 'next-auth/react';
 import useSWR, { useSWRConfig } from 'swr';
-// import Link from 'next/link'
+import Link from 'next/link'
 import fetcher from 'lib/fetcher';
 import { Form, FormState } from 'lib/types';
 import SuccessMessage from 'components/SuccessMessage';
@@ -52,12 +52,6 @@ export default function Guestbook({ fallbackData }) {
   const { data: session } = useSession();
   const [isLoadingGoogle, setIsLoadingGoogle] = useState<boolean>();
   const [isLoadingGithub, setIsLoadingGithub] = useState<boolean>();
-  const [visible, setVisible] = React.useState(false);
-  const handler = () => setVisible(true);
-  const closeHandler = () => {
-    setVisible(false);
-    console.log("closed");
-  };
   const { mutate } = useSWRConfig();
   const [form, setForm] = useState<FormState>({ state: Form.Initial });
   const inputEl = useRef(null);
@@ -108,10 +102,9 @@ export default function Guestbook({ fallbackData }) {
         {!session && (
           <>
           <div className='flex flex-col sm:flex-row sm:gap-5'>
-          <a
-            href="/api/auth/signin/github"
-            className="flex items-center justify-center my-4 px-3 h-10 rounded-lg font-bold bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 gap-1 w-48"
-            onClick={(e) => {
+          <Button bordered auto shadow
+            component={Link} to={`/api/auth/signin/github`}
+              onClick={(e) => {
               e.preventDefault();
               signIn('github');
               setIsLoadingGithub(true);
@@ -127,7 +120,7 @@ export default function Guestbook({ fallbackData }) {
            <svg  className='fill-current w-5 h-5 mr-2 mb-1' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
            </>
           )}
-          </a>
+          </Button>
           <a
           href="/api/auth/signin/google"
           className="flex items-center px-3 justify-center my-4 font-bold h-10 bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-lg transition-colors duration-150 border border-transparent gap-1 w-48"
