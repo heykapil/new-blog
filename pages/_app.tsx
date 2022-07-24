c4e145322485eeda23dbaf6618d27613faf75f6b
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2022-07-24 17:52:28 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,37 @@
 import 'styles/global.css';
 import 'katex/dist/katex.min.css'
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import DarkTheme from './themes/DarkTheme';
 import { SessionProvider } from 'next-auth/react';
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider, theme as NextUITheme } from "@nextui-org/react"
+
+const lightTheme = createTheme({
+  type: 'light',
+  theme: { ...NextUITheme }
+
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: { ...DarkTheme }
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <ThemeProvider attribute="class">
+      <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}>
          <NextUIProvider>
           <Component {...pageProps} />
           </NextUIProvider>
-      </ThemeProvider>
+      </NextThemesProvider>
     </SessionProvider>
   );
 }
